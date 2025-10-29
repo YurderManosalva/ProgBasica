0297f553a422cfc02a0eba5f0250070fc45d8b2c
Yurder Manosalva <yurdermanosalva01@gmail.com>
2025-10-28 20:35:38 -0500

    se agrego la logica js

--- js/verpassword.js
+++ js/verpassword.js
@@ -1,6 +1,9 @@
-function validar() {
-    Swal.fire({
-        title: "Contraseña correcta",
-        icon: "success"
-    });
-}
+const password = document.getElementById("password");
+const toggle_password = document.getElementById("toggle_password");
+
+toggle_password.addEventListener("click", () => {
+    const type = password.type === "password" ? "text" : "password";
+    password.type = type;
+    toggle_password.classList.toggle("fa-eye")
+    toggle_password.classList.toggle("fa-eye-slash")
+})
